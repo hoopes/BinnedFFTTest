553728b5b02009c827d0570aa48fd5fc50e9a2e7
Matthew Hoopes <matthew.hoopes@gmail.com>
2024-01-14 09:13:14 -0500

    Initial Commit

---
+++ BinnedFFTView/BinnedFFTViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinnedFFTViewApp.swift
+//  BinnedFFTView
+//
+//  Created by Matthew Hoopes on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct BinnedFFTViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
